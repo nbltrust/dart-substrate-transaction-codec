525ce13aec4cf106869b64adf24e0212e5cb2c7a
Steven <capricorn0113@gmail.com>
2021-07-07 18:22:29 +0800

    invoke local external services to decode payload

--- lib/src/transaction.dart
+++ lib/src/transaction.dart
@@ -1,25 +1,47 @@
 library substrate_codec.transaction;
 
+import 'dart:convert';
+import 'dart:io' show ContentType, HttpHeaders;
 import 'dart:typed_data';
 import 'package:convert/convert.dart';
 import 'package:quiver/collection.dart';
 import 'package:pointycastle/digests/blake2b.dart';
+import 'package:sync_http/sync_http.dart';
+
+String strip0x(String hex) {
+  if (hex.startsWith('0x')) return hex.substring(2);
+  return hex;
+}
 
 class PolkaTransaction extends DelegatingMap {
   final delegate = new Map<String, dynamic>();
 
-  PolkaTransaction(String payload) {
+  PolkaTransaction(String payload, dynamic decoded) {
     this.delegate['payload'] = payload;
-    // this.delegate['era'] = extrinsics.era.toJson();
-    // this.delegate['nonce'] = extrinsics.nonce.toJson();
-    // this.delegate['tip'] = extrinsics.tip.toJson();
-    // this.delegate['callIndex'] = [extrinsics.call.module_index, extrinsics.call.function_index];
-    // this.delegate['args'] = extrinsics.call.argValues.map((k, v) => MapEntry(k, v.toJson()));
+
+    if (decoded['code'] != 0) {
+      return;
+    }
+
+    final result = decoded['result'];
+    this.delegate['method'] = result['method'];
+    this.delegate['tip'] = result['tip'];
     // print(this.delegate);
   }
 
   factory PolkaTransaction.deserialize(String payload) {
-    PolkaTransaction tx = new PolkaTransaction(payload);
+    final request = SyncHttpClient.postUrl(Uri.http('localhost:3000', '/substrate/decode'));
+    request.headers.contentType = ContentType('application', 'json', charset: 'utf-8');
+    request.write(json.encode({
+      'signingPayload': '0x' + strip0x(payload),
+    }));
+    request.headers.add(HttpHeaders.acceptHeader, 'application/json');
+    request.headers.add(HttpHeaders.cacheControlHeader, 'no-cache');
+
+    final response = request.close();
+    // print(response.body);
+
+    PolkaTransaction tx = new PolkaTransaction(strip0x(payload), json.decode(response.body));
 
     return tx;
   }
--- test/transaction_test.dart
+++ test/transaction_test.dart
@@ -4,15 +4,14 @@ import 'package:substrate_codec/substrate_codec.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("test", () {
-    // final rawPayload =
-    //     "0x0500913d5f22d531d1ab7f7ec337f50a5e856e83d2c145f009f0b23514c088f86a270700e40b5402001800160000000400000097094e6fb7fb6066cacbf47074a16ce8d4f38a9d02493635f543e2da6d20d57597094e6fb7fb6066cacbf47074a16ce8d4f38a9d02493635f543e2da6d20d575";
-    // final rawPayload =
-    //     "0x5d028400fcc4910cb536b4333db4bccb40e2cf6427b4766518e754b91e70c97e4a87dbb300d99ffe3e610ad234e1414bda5831395a6df9098bf80b01561ce89a5065ae89d5c10e1619c6c99131b0bea4fb73ef04d07c07770e2ae9df5c325c331769ccb300a90b11010700ac23fc06060000495e1e506f266418af07fa0c5c108dd436f2faa59fe7d9e54403779f5bbd77180bc01eb1fc185f";
+  test("test deserialize tx", () {
     final rawPayload =
-        "a4040000e9cd2a1848079a5d34669b15fa3e3d969562814719981f6aee26b29288915e360700e40b5402750204006e23000005000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423ee5eb8c9f857d9cdb10c1c86a80d18cefea40d7c1586e3cb64bf7a1af6f206f23";
-    final tx = PolkaTransaction.deserialize(rawPayload);
-    tx.hashToSign();
-    expect(1, 1);
+        "0xa00503da04de6cd781c98acf0693dfb97c11011938ad22fcc476ed0089ac5aec3fe243070088526a74e5000800fb03000001000000e3777fa922cafbff200cadeaea1a76bd7898ad5b89f7848999058b50e715f6361fc7493f3c1e9ac758a183839906475f8363aafb1b1d3e910fe16fab4ae1b582";
+    PolkaTransaction tx = PolkaTransaction.deserialize(rawPayload);
+
+    expect(tx['method']['pallet'], 'balances');
+    expect(tx['method']['name'], 'transferKeepAlive');
+    expect(tx['method']['args']['dest'], '15vrtLsCQFG3qRYUcaEeeEih4JwepocNJHkpsrqojqnZPc2y');
+    expect(tx['method']['args']['value'], '500000000000');
   });
 }
